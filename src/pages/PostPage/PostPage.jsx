3e380f77fc54cfe86f80c6df94633320c7c31653
maalireza51 <maalireza51@gmail.com>
2022-12-15 14:32:03 +0330

    add useLocation() to post page

--- src/pages/PostPage/PostPage.jsx
+++ src/pages/PostPage/PostPage.jsx
@@ -1,5 +1,5 @@
-import { useEffect, useState } from "react";
-import { Link, useParams } from "react-router-dom";
+import { useEffect, useReducer, useState } from "react";
+import { Link, useLocation, useParams } from "react-router-dom";
 import Layout from "../../components/Layout";
 import { get } from "../../Services/HttpClient";
 import {getComments} from "./getComments"
@@ -9,6 +9,7 @@ export default function PostPage() {
   const id = useParams();
   const [elements, setElements] = useState([])
   const [comments, setComments] = useState([])
+  const location = useLocation();
 
   useEffect(() => {
     get("posts").then(res => {
@@ -17,7 +18,7 @@ export default function PostPage() {
     getComments(id.id).then(res => {
       setComments(res.data)
     })
-  }, [])
+  }, [location])
 
   const list = elements.map((element, index) => (
     <li key={element.id}><Link to={`../comment/${element.id}`}>{element.title}</Link></li>
